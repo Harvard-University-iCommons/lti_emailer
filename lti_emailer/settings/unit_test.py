395eb41a197d73775544cac3f1246ca81c4d3483
Douglas Hall <douglas_hall@harvard.edu>
2015-04-15 13:20:57 +0000

    Removing live server test config

--- lti_emailer/settings/unit_test.py
+++ lti_emailer/settings/unit_test.py
@@ -19,23 +19,3 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache'
     },
 }
-
-"""
-Unit tests should be able to run without talking to external services like
-redis/elasticache.  Let's disable the caching middleware, and use the db
-to store session data.
-"""
-MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-MIDDLEWARE_CLASSES[MIDDLEWARE_CLASSES.index('cached_auth.Middleware')] = \
-    'django.contrib.auth.middleware.AuthenticationMiddleware'
-MIDDLEWARE_CLASSES = tuple(MIDDLEWARE_CLASSES)
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-
-"""
-Don't assume we'll have oauth credentials set for us in secure.py, set
-them up here instead.
-"""
-SECURE_SETTINGS.setdefault('lti_oauth_credentials', {}).update({'foo': 'bar'})
-if not LTI_OAUTH_CREDENTIALS:
-    LTI_OAUTH_CREDENTIALS = {}
-LTI_OAUTH_CREDENTIALS.update({'foo': 'bar'})
