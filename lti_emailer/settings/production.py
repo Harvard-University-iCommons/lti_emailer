7adf8a12fd7aff93d92665f616d0457ca2f2e9ba
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2015-04-19 21:24:19 -0400

    Hardcode default db backend to postgres.  Add cookie and environment secure settings.  Update canvas api url to be base canvas url for reuse opportunity with other projects.  Add a cache key_prefix.  Use django cache backend for sessions instead of redis_sessions backend (since cache backend is already redis).  Add log_root setting.  Simplify other environment setting files.  Update secure.py example file.

--- lti_emailer/settings/production.py
+++ lti_emailer/settings/production.py
@@ -1,17 +1,4 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-TEMPLATE_DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
-INSTALLED_APPS += ('gunicorn',)
-
-GUNICORN_CONFIG = 'gunicorn_prod.py'
-
-SESSION_COOKIE_SECURE = True
-
 # Allow the mailing_list app to allow any user to be added to listserv
 IGNORE_WHITELIST = True
