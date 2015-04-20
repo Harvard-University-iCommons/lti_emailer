c067607058da5c9cdf71b4f651d6ab9d5e0521e7
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2015-04-19 22:21:10 -0400

    Add lti_emailer prefix to urls so shared web server can proxy to project.

--- lti_emailer/urls.py
+++ lti_emailer/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^lti_auth_error/', 'lti_emailer.views.lti_auth_error', name='lti_auth_error'),
-    url(r'^tool_config$', 'lti_emailer.views.tool_config', name='tool_config'),
-    url(r'^lti_launch$', 'lti_emailer.views.lti_launch', name='lti_launch'),
-    url(r'^mailing_list/', include('mailing_list.urls', namespace='mailing_list')),
+    url(r'^lti_emailer/lti_auth_error/', 'lti_emailer.views.lti_auth_error', name='lti_auth_error'),
+    url(r'^lti_emailer/tool_config$', 'lti_emailer.views.tool_config', name='tool_config'),
+    url(r'^lti_emailer/lti_launch$', 'lti_emailer.views.lti_launch', name='lti_launch'),
+    url(r'^lti_emailer/mailing_list/', include('mailing_list.urls', namespace='mailing_list')),
 )
