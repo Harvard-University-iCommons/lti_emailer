0adda6a4018d604755d5438c6e3c65694ab9d070
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-07-31 16:56:33 -0400

    some cleanup

--- mailing_list/models.py
+++ mailing_list/models.py
@@ -185,8 +185,6 @@ class MailingList(models.Model):
     def send_mail(self, sender_address, to_address, subject='', text='', html=''):
         logger.debug("   in send_mail: sender_address=%s, to_address=%s, mailing_list.address=%s "
                      % (sender_address, to_address, self.address))
-        formatted_from_address = sender_address + '<'+self.address+'>'
-        logger.debug("formatted_from_address=%s " % formatted_from_address)
         listserv_client.send_mail(sender_address, to_address,  self.address, subject, text, html)
 
     def sync_listserv_membership(self):
