4676e972e8d5bed425e2d16f49faafa1551d08f7
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-08-04 12:59:15 -0400

    printing mailing list addresses

--- mailgun/route_handlers.py
+++ mailgun/route_handlers.py
@@ -59,6 +59,8 @@ def handle_mailing_list_email_route(request):
     # Always include teaching staff addresses with members addresses, so that they can email any list in the course
     teaching_staff_addresses = ml.teaching_staff_addresses
     member_addresses = set([m['address'] for m in ml.members] + teaching_staff_addresses)
+    logger.debug(" here' the member_addresses")
+    logger.debug(member_addresses)
     bounce_back_email_template = None
     if ml.access_level == MailingList.ACCESS_LEVEL_MEMBERS and sender not in member_addresses:
         logger.info(
