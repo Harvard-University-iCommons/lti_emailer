21b06658ee7226ebdd139395a6240df4aceddec7
Dave Bonner <david_bonner@harvard.edu>
2015-08-06 10:52:39 -0400

    fix bounce send

--- mailgun/route_handlers.py
+++ mailgun/route_handlers.py
@@ -97,8 +97,8 @@ def handle_mailing_list_email_route(request):
             'message_body': message_body
         }))
         subject = "Undeliverable mail"
-        ml.send_mail(sender_address.full_spec(), member_address, subject,
-                     html=content)
+        ml.send_mail(ml.address, sender_address.full_spec(),
+                     subject, html=content)
     else:
         # try to prepend [SHORT TITLE] to subject, keep going if lookup fails
         try:
