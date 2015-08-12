2057bbcf4b42c5e636af6eeb233dbbb9148ce5e3
Douglas Hall <douglas_hall@harvard.edu>
2015-08-12 16:06:12 +0000

    Fix bounceback to address

--- mailgun/route_handlers.py
+++ mailgun/route_handlers.py
@@ -93,7 +93,7 @@ def handle_mailing_list_email_route(request):
             'message_body': message_body
         }))
         subject = "Undeliverable mail"
-        ml.send_mail('', ml.address, sender_address.full_spec(), subject, html=content)
+        ml.send_mail('', ml.address, sender_address.address, subject, html=content)
     else:
         # try to prepend [SHORT TITLE] to subject, keep going if lookup fails
         try:
