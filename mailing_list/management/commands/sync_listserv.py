9afc504f801e41e2883569241856d9efb57b0236
Dave Bonner <david_bonner@harvard.edu>
2015-08-24 10:55:43 -0400

    remove unused imports, unnecessary __init__

--- mailing_list/management/commands/sync_listserv.py
+++ mailing_list/management/commands/sync_listserv.py
@@ -1,9 +1,7 @@
 import logging
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from mailing_list.models import MailingList
 from mailing_list.tasks import course_sync_listserv
 
 
@@ -12,9 +10,6 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'Synchronizes mailing lists created for canvas courses with the enrollment list for each course'
-
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
         logger.info("Beginning sync_listserv job for canvas_course_ids %s", args)
