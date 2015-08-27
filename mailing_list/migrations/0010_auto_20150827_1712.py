15fcf0d407063dd10125ac2792136f8bd5653777
Douglas Hall <douglas_hall@harvard.edu>
2015-08-27 17:13:19 +0000

    Added migration

---
+++ mailing_list/migrations/0010_auto_20150827_1712.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mailing_list', '0009_auto_20150727_1530'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='unsubscribed',
+            unique_together=set([]),
+        ),
+        migrations.RemoveField(
+            model_name='unsubscribed',
+            name='mailing_list',
+        ),
+        migrations.RemoveField(
+            model_name='mailinglist',
+            name='subscriptions_updated',
+        ),
+        migrations.DeleteModel(
+            name='Unsubscribed',
+        ),
+    ]
