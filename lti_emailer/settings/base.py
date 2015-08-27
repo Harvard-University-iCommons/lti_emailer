5de6b699fa887977d41903cc172e3b79aa8ea8f8
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-08-27 10:27:42 -0400

    Decreased timeout for testing and printing some stats. Turned down logging

--- lti_emailer/settings/base.py
+++ lti_emailer/settings/base.py
@@ -127,8 +127,8 @@ CACHES = {
         'KEY_PREFIX': 'lti_emailer',  # Provide a unique value for shared cache
         # See following for default timeout (5 minutes as of 1.7):
         # https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-CACHES-TIMEOUT
-        #Increasing the timeout to 60 mins
-        'TIMEOUT': SECURE_SETTINGS.get('default_cache_timeout_secs', 3600),
+        # Temporarily decreasing teh cache timeout for testing
+        'TIMEOUT': SECURE_SETTINGS.get('default_cache_timeout_secs', 5),
     },
 }
 
--- lti_emailer/settings/test.py
+++ lti_emailer/settings/test.py
@@ -1,1 +1,4 @@
 from .aws import *
+
+#Temporarily reducing  the logging level to INFO tfor testing
+_DEFAULT_LOG_LEVEL = 'INFO'
