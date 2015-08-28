f6a975e340cf85175c907f7269514338a76d1a2b
Mysore, Sapna <sapna_mysore@harvard.edu>
2015-08-28 14:18:32 -0400

    temp change to log level

--- lti_emailer/settings/demo.py
+++ lti_emailer/settings/demo.py
@@ -2,3 +2,6 @@ from .aws import *
 
 # Allow the mailing_list app to allow any user to be added to listserv
 IGNORE_WHITELIST = True
+
+#Temporarily reducing  the logging level to INFO tfor testing
+_DEFAULT_LOG_LEVEL = 'INFO'
