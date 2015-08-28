a36b79268a74922c533ca76a4b7de2ee4a9bca70
Douglas Hall <douglas_hall@harvard.edu>
2015-08-28 14:24:34 +0000

    Added unit test for delete functionality

--- mailing_list/tests.py
+++ mailing_list/tests.py
@@ -12,76 +12,132 @@ class MailingListModelTests(TestCase):
     ]
     longMessage = True
 
+    def setUp(self):
+        self.sections = [{
+            'id': 1582,
+            'name': 'section name 1',
+            'sis_section_id': 334562
+        }, {
+            'id': 1583,
+            'name': 'section name 2',
+            'sis_section_id': None
+        }]
+
     @patch('mailing_list.models.canvas_api_client.get_enrollments')
     @patch('mailing_list.models.canvas_api_client.get_sections')
     def test_get_or_create_or_delete_mailing_lists_for_canvas_course_id_with_existing_list(self, mock_get_sections,
                                                                                            mock_get_enrollments):
-        mock_get_sections.return_value = [{
-            'id': 1582,
-            'name': 'section name',
-            'sis_section_id': 334562
-        }]
+        mock_get_sections.return_value = self.sections
         mock_get_enrollments.return_value = []
 
         result = MailingList.objects.get_or_create_or_delete_mailing_lists_for_canvas_course_id(3716)
-        address = settings.LISTSERV_ADDRESS_FORMAT.format(
+        address_1 = settings.LISTSERV_ADDRESS_FORMAT.format(
             canvas_course_id=3716,
             section_id=1582
+        )
+        address_2 = settings.LISTSERV_ADDRESS_FORMAT.format(
+            canvas_course_id=3716,
+            section_id=1583
         )
 
         self.assertEqual(result, [{
             'id': 1,
             'canvas_course_id': 3716,
             'section_id': 1582,
-            'name': 'section name',
-            'address': address,
+            'name': 'section name 1',
+            'address': address_1,
             'access_level': 'members',
             'members_count': 0,
             'is_primary_section': True
+        }, {
+            'id': 2,
+            'canvas_course_id': 3716,
+            'section_id': 1583,
+            'name': 'section name 2',
+            'address': address_2,
+            'access_level': 'members',
+            'members_count': 0,
+            'is_primary_section': False
         }])
 
     @patch('mailing_list.models.canvas_api_client.get_enrollments')
     @patch('mailing_list.models.canvas_api_client.get_sections')
     def test_get_or_create_or_delete_mailing_lists_for_canvas_course_id_with_new_list(self, mock_get_sections,
                                                                                       mock_get_enrollments):
-        mock_get_sections.return_value = [{
-            'id': 1583,
-            'name': 'section name 1',
-            'sis_section_id': 334562
-        }, {
+        sections = list(self.sections)
+        sections.append({
             'id': 1584,
-            'name': 'section name 2',
+            'name': 'section name 3',
             'sis_section_id': None
-        }]
+        })
+        mock_get_sections.return_value = sections
         mock_get_enrollments.return_value = []
 
         result = MailingList.objects.get_or_create_or_delete_mailing_lists_for_canvas_course_id(3716)
         address_1 = settings.LISTSERV_ADDRESS_FORMAT.format(
             canvas_course_id=3716,
-            section_id=1583
+            section_id=1582
         )
         address_2 = settings.LISTSERV_ADDRESS_FORMAT.format(
+            canvas_course_id=3716,
+            section_id=1583
+        )
+        address_3 = settings.LISTSERV_ADDRESS_FORMAT.format(
             canvas_course_id=3716,
             section_id=1584
         )
 
-        self.assertIn({
-            'id': 2,
+        self.assertEqual([{
+            'id': 1,
             'canvas_course_id': 3716,
-            'section_id': 1583,
+            'section_id': 1582,
             'name': 'section name 1',
             'address': address_1,
             'access_level': 'members',
             'members_count': 0,
             'is_primary_section': True
-        }, result)
-        self.assertIn({
-            'id': 3,
+        }, {
+            'id': 2,
             'canvas_course_id': 3716,
-            'section_id': 1584,
+            'section_id': 1583,
             'name': 'section name 2',
             'address': address_2,
             'access_level': 'members',
             'members_count': 0,
             'is_primary_section': False
-        }, result)
+        }, {
+            'id': 3,
+            'canvas_course_id': 3716,
+            'section_id': 1584,
+            'name': 'section name 3',
+            'address': address_3,
+            'access_level': 'members',
+            'members_count': 0,
+            'is_primary_section': False
+        }], result)
+
+    @patch('mailing_list.models.canvas_api_client.get_enrollments')
+    @patch('mailing_list.models.canvas_api_client.get_sections')
+    def test_get_or_create_or_delete_mailing_lists_for_canvas_course_id_with_deleted_section(self, mock_get_sections,
+                                                                                             mock_get_enrollments):
+        sections = list(self.sections)
+        del sections[1]
+        mock_get_sections.return_value = sections
+        mock_get_enrollments.return_value = []
+
+        result = MailingList.objects.get_or_create_or_delete_mailing_lists_for_canvas_course_id(3716)
+        address_1 = settings.LISTSERV_ADDRESS_FORMAT.format(
+            canvas_course_id=3716,
+            section_id=1582
+        )
+
+        self.assertEqual([{
+            'id': 1,
+            'canvas_course_id': 3716,
+            'section_id': 1582,
+            'name': 'section name 1',
+            'address': address_1,
+            'access_level': 'members',
+            'members_count': 0,
+            'is_primary_section': True
+        }], result)
