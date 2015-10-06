9fbb56a4817d3dd26fd53ec9b967b83704671fe1
Douglas Hall <douglas_hall@harvard.edu>
2015-10-06 20:37:58 +0000

    Testing another fix

--- mailgun/listserv_client.py
+++ mailgun/listserv_client.py
@@ -246,10 +246,10 @@ class MailgunClient(object):
 
         files = []
         if attachments:
-            files.extend([('attachment', (f.name.encode('latin-1'), f, f.content_type))
+            files.extend([('attachment', (f.name.encode('utf-8'), f, f.content_type))
                               for f in attachments])
         if inlines:
-            files.extend([('inline', (f.name.encode('latin-1'), f, f.content_type))
+            files.extend([('inline', (f.name.encode('utf-8'), f, f.content_type))
                               for f in inlines])
 
         logger.info("files is %s", files)
