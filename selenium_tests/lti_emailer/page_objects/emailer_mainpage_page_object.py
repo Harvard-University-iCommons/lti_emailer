988acf0576b9c113a9195d87a1fad1ec4b57ab7c
hktest <hiukei_chow@harvard.edu>
2016-04-11 15:53:17 -0400

    - updated locator element and added in a wait for the unauthorized message. - updated test data to reflect masquerade user ID.  some of the IDs used for the canvas.icommons instance (now deprecated) so it is now pointing to users who aren't the intended test ID in the course.

--- selenium_tests/lti_emailer/page_objects/emailer_mainpage_page_object.py
+++ selenium_tests/lti_emailer/page_objects/emailer_mainpage_page_object.py
@@ -1,20 +1,24 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+
 from selenium.webdriver.common.by import By
 from selenium_tests.lti_emailer.page_objects.emailer_base_page_object import EmailerBasePageObject
 
 
-class EmailerMainPageLocators(object):
+
+class Locators(object):
     UNAUTHORIZED_MESSAGE = (By.ID, "unauthorized_message")
     MEMBER_LINK = (By.PARTIAL_LINK_TEXT, "members")
     MAILING_LISTS_ID = (By.ID, "course-mailing-lists")
 
 class EmailerMainPage(EmailerBasePageObject):
-    page_loaded_locator = EmailerMainPageLocators.MAILING_LISTS_ID
+    page_loaded_locator = Locators.MAILING_LISTS_ID
 
     def is_authorized(self):
 
         try:
-            self.find_element(*EmailerMainPageLocators.UNAUTHORIZED_MESSAGE)
+            WebDriverWait(self._driver, 60).until(
+               self.find_element(*Locators.UNAUTHORIZED_MESSAGE))
         except NoSuchElementException:
 
             #unauthorized message not found, we should see main page header
@@ -24,11 +28,11 @@ class EmailerMainPage(EmailerBasePageObject):
         return False
 
     def get_member_link(self):
-        element = self.find_element(*EmailerMainPageLocators.MEMBER_LINK)
+        element = self.find_element(*Locators.MEMBER_LINK)
         return element
 
     def select_member_link(self):
         """
         select the course info link element and click it
         """
-        self.find_element(*EmailerMainPageLocators.MEMBER_LINK).click()
+        self.find_element(*Locators.MEMBER_LINK).click()
