dda271d9b8e8899c14c3bc667ea11c411d023ccd
Yates, Elliott <elliott_yates@harvard.edu>
2016-04-11 21:53:32 -0400

    tlt-2577: fixes issue with selenium permissions tests

--- selenium_tests/lti_emailer/page_objects/emailer_mainpage_page_object.py
+++ selenium_tests/lti_emailer/page_objects/emailer_mainpage_page_object.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 from selenium_tests.lti_emailer.page_objects.emailer_base_page_object import EmailerBasePageObject
 
 
-
 class Locators(object):
     UNAUTHORIZED_MESSAGE = (By.ID, "unauthorized_message")
     MEMBER_LINK = (By.PARTIAL_LINK_TEXT, "members")
     MAILING_LISTS_ID = (By.ID, "course-mailing-lists")
+
 
 class EmailerMainPage(EmailerBasePageObject):
     page_loaded_locator = Locators.MAILING_LISTS_ID
@@ -17,11 +17,11 @@ class EmailerMainPage(EmailerBasePageObject):
     def is_authorized(self):
 
         try:
-            WebDriverWait(self._driver, 60).until(
-               self.find_element(*Locators.UNAUTHORIZED_MESSAGE))
+            self.focus_on_tool_frame()
+            self.find_element(*Locators.UNAUTHORIZED_MESSAGE)
         except NoSuchElementException:
 
-            #unauthorized message not found, we should see main page header
+            # unauthorized message not found, we should see main page header
             return True
 
         # we found the unauthorized message, so we're explicitly unauthorized
