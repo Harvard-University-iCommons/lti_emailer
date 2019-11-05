42a87d13bd956d48ebd4738d6e0de6ad17a4854a
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-05 16:37:03 -0500

    Add trailing slash to urls

--- mailing_list/urls.py
+++ mailing_list/urls.py
@@ -4,10 +4,10 @@ from mailing_list import views, api
 
 
 urlpatterns = [
-    path('admin_index', views.admin_index, name='admin_index'),
-    path('list_members', views.list_members, name='list_members_no_id'),
-    re_path(r'^list_members/(?P<section_id>\d+)$', views.list_members, name='list_members'),
-    path('api/lists', api.lists, name='api_lists'),
-    re_path(r'^api/lists/(?P<mailing_list_id>\d+)/set_access_level$', api.set_access_level, name='api_lists_set_access_level'),
-    path('api/course_settings', api.get_or_create_course_settings, name='api_course_settings'),
+    path('admin_index/', views.admin_index, name='admin_index'),
+    path('list_members/', views.list_members, name='list_members_no_id'),
+    re_path(r'^list_members/(?P<section_id>\d+)/$', views.list_members, name='list_members'),
+    path('api/lists/', api.lists, name='api_lists'),
+    re_path(r'^api/lists/(?P<mailing_list_id>\d+)/set_access_level/$', api.set_access_level, name='api_lists_set_access_level'),
+    path('api/course_settings/', api.get_or_create_course_settings, name='api_course_settings'),
 ]
