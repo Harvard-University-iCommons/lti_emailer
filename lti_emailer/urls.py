44557f10b346cb43abf0abba663f3f780e1b63fe
Chris-Thornton-Harvard <christopher_thornton@harvard.edu>
2019-11-18 10:26:56 -0500

    URL pattern tweaks, converted a subset to re_path

--- lti_emailer/urls.py
+++ lti_emailer/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('lti_auth_error/', views.lti_auth_error, name='lti_auth_error'),
     path('tool_config/', views.tool_config, name='tool_config'),
     path('lti_launch', views.lti_launch, name='lti_launch'),
-    path('mailing_list/', include(('mailing_list.urls', 'mailing_list'), namespace='mailing_list')),
-    path('mailgun/', include(('mailgun.urls', 'mailgun'), namespace='mailgun')),
+    re_path(r'^mailing_list/', include(('mailing_list.urls', 'mailing_list'), namespace='mailing_list')),
+    re_path(r'^mailgun/', include(('mailgun.urls', 'mailgun'), namespace='mailgun')),
     path('not_authorized/', icommons_ui_views.not_authorized, name="not_authorized"),
 ]
 
--- mailgun/urls.py
+++ mailgun/urls.py
@@ -4,7 +4,7 @@ from mailgun import views, route_handlers
 
 
 urlpatterns = [
-    path('handle_mailing_list_email_route/', route_handlers.handle_mailing_list_email_route, name='handle_mailing_list_email_route'),
+    re_path(r'^handle_mailing_list_email_route/', route_handlers.handle_mailing_list_email_route, name='handle_mailing_list_email_route'),
     path('auth_error/', views.auth_error, name='auth_error'),
-    path('log_post_data/', views.log_post_data, name='log_post_data'),
+    re_path(r'^log_post_data/', views.log_post_data, name='log_post_data'),
 ]
