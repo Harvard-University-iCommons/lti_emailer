f264f93616d471cb13eff5cd23c33dda35f5d223
Haydn <haydn_ady@harvard.edu>
2022-08-18 13:22:56 -0400

    Test logging

--- mailgun/route_handlers.py
+++ mailgun/route_handlers.py
@@ -116,7 +116,6 @@ def _handle_recipient(request, recipient, user_alt_email_cache):
     subject = request.POST.get('subject')
     to_list = addresslib_address.parse_list(request.POST.get('To'))
 
-    logger.info(f'attachments: {attachments}, inlines: {inlines}')
     logger.debug('Handling recipient %s, from %s, subject %s, message id %s',
                  recipient, sender, subject, message_id)
 
@@ -407,7 +406,8 @@ def _get_attachments_inlines(request):
         else:
             attachments.append(file_)
 
-    logger.info(f'attachments: {attachments}, inlines: {inlines}')
+    attachments_metadata = request.POST.get('attachments', '')
+    logger.info(f'attachments: {attachments}, inlines: {inlines}, attachments_metadata: {attachments_metadata}')
 
     return attachments, inlines
 
