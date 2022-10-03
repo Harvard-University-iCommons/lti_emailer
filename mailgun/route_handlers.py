76a2234bbb8ae88c338d944bb76fca4b5bee3123
Haydn <haydn_ady@harvard.edu>
2022-10-03 11:49:41 -0400

    WIP

--- mailgun/route_handlers.py
+++ mailgun/route_handlers.py
@@ -416,16 +416,18 @@ def _get_attachments_inlines(request, sender, recipient, subject, body_plain, bo
         try:
             file_ = request.FILES[attachment_name]
         except KeyError:
-            attachment_content = request.POST.get(attachment_name, '')
+            try:
+                attachment_content = request.POST.get(attachment_name, '')
+            except Exception as e:
+                logger.info("Failed to retrieve .eml attachment")
 
             logger.info("attachment_content", extra=attachment_content)
 
             if attachment_content:
-                fp = tempfile.TemporaryFile()
-                fp.write(attachment_content)
-                attachments.append(fp)
-                fp.close()
-
+                # fp = tempfile.TemporaryFile()
+                # fp.write(attachment_content)
+                # attachments.append(fp)
+                # fp.close()
                 logger.info("attachments after saving temp file", extra=attachments)
 
 
